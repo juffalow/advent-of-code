6c15db113a41554adf86a0e439fa4598d5a62e9a
Matej Jellus <juffalow@juffalow.com>
2022-12-05 18:39:30 +0100

    Add solution for day 1 in 2022

---
+++ 2022/1/second_solution.py
@@ -0,0 +1,18 @@
+import sys
+
+elves = []
+elf = 1
+calories = 0
+
+for line in sys.stdin:
+  if line.strip() == "":
+    elves.append(calories)
+    elf += 1
+    calories = 0
+    continue
+
+  calories += int(line)
+
+elves.sort(reverse = True)
+
+print(elves[0] + elves[1] + elves[2])
